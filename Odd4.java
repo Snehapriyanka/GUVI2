c9badde07598d70990df67b52797446f3daa8016
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-20 11:02:01 +0530

    Add files via upload

---
+++ Odd4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Odd4 {
+
+	public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a=s.nextInt();
+for(int j=n+1;j<a;j++) {
+if(j%2==1) 
+	System.out.println(j+"odd");
+
+	}
+	}
+	}
+
+
