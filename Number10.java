0690f562665bcfa27498a159158e656182d57053
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-07-20 18:38:47 +0530

    Add files via upload

---
+++ Number10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Number10 {
+
+	public static void main(String[] args) {
+	int n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(int i=1;i<=5;i++) {	
+	System.out.print(" "+n*i);
+	}
+
+	}
+}
